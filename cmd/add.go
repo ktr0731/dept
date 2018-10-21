2b96a518b95dc0f5b72d89be1b8dcb672160352e
ktr0731 <ktr@syfm.me>
2018-10-21 12:23:11 +0900

    use mitchellh/cli

---
+++ cmd/add.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/k0kubun/pp"
+	"github.com/mitchellh/cli"
+)
+
+type addCommand struct{}
+
+func (c *addCommand) Help() string {
+	return "Usage: dept add <url>"
+}
+
+func (c *addCommand) Synopsis() string {
+	return "Add new CLI tool as a dependency"
+}
+
+func (c *addCommand) Run(args []string) int {
+	pp.Println(args)
+	if len(args) != 1 {
+		fmt.Println(c.Help())
+		return 1
+	}
+
+	ctx := context.Background()
+
+	path := args[0]
+
+	dir, err := filepath.Abs("./vendor")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("mkdir: %s\n", dir)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Fatalln(err)
+	}
+
+	newGOPATH := dir
+	fmt.Printf("set GOPATH to vendor: %s\n", newGOPATH)
+	gopath := os.Getenv("GOPATH")
+	os.Setenv("GOPATH", newGOPATH)
+	defer os.Setenv("GOPATH", gopath)
+
+	// 依存をすべて取得し、対象リポジトリで `dep ensure` する
+	fmt.Printf("go get repository...: %s\n", path)
+	b, err := exec.CommandContext(ctx, "go", "get", "-d", path).CombinedOutput()
+	fmt.Println(string(b))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dist := filepath.Join("vendor", "src", path)
+	fmt.Printf("change directory: %s\n", dist)
+	os.Chdir(dist)
+
+	if _, err := os.Stat("Gopkg.toml"); os.IsNotExist(err) {
+		log.Fatalln("Gopkg.toml not found, abort")
+	}
+
+	fmt.Printf("dep ensure: %s\n", dist)
+	if err := exec.CommandContext(ctx, "dep", "ensure").Run(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("go install", path)
+	b, err = exec.CommandContext(ctx, "go", "install", path).CombinedOutput()
+	fmt.Println(string(b))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return 0
+}
+
+func Add() (cli.Command, error) {
+	return &addCommand{}, nil
+}
--- main.go
+++ main.go
@@ -1,56 +1,23 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
+
+	"github.com/ktr0731/dept/cmd"
+	"github.com/mitchellh/cli"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalln("Usage: dept <url>")
+	app := cli.NewCLI("dept", "0.1.0")
+	app.Commands = map[string]cli.CommandFactory{
+		"add": cmd.Add,
 	}
-
-	ctx := context.Background()
-
-	sp := strings.Split(os.Args[1], " ")
-	path := filepath.Join(append([]string{"vendor"}, sp...)...)
-	fmt.Printf("mkdir: %s\n", path)
-	if err := os.MkdirAll(path, 0755); err != nil {
-		log.Fatalln(err)
-	}
-
-	newGOPATH, err := filepath.Abs("./vendor")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Printf("set GOPATH to vendor: %s\n", newGOPATH)
-	gopath := os.Getenv("GOPATH")
-	os.Setenv("GOPATH", newGOPATH)
-	defer os.Setenv("GOPATH", gopath)
-
-	// 依存をすべて取得し、対象リポジトリで `dep ensure` する
-	fmt.Printf("go get repository...: %s\n", os.Args[1])
-	b, err := exec.CommandContext(ctx, "go", "get", "-d", os.Args[1]).CombinedOutput()
-	fmt.Println(string(b))
+	app.Args = os.Args[1:]
+	code, err := app.Run()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	dist := fmt.Sprintf("vendor/%s", os.Args[1])
-	fmt.Printf("dep ensure: %s\n", dist)
-	os.Chdir(dist)
-
-	exec.CommandContext(ctx, "dep", "ensure")
-
-	// fmt.Println("go install", os.Args[1])
-	// b, err = exec.CommandContext(ctx, "go", "install", os.Args[1]).CombinedOutput()
-	// fmt.Println(string(b))
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	os.Exit(code)
 }
