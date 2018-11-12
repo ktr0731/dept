8f0de886fa3f5bcefade6638fcf8cb0ba8b4e1f8
ktr0731 <ktr@syfm.me>
2018-11-12 12:23:48 +0900

    set GO111MODULE=on if Go version is 1.11

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"strings"
 
+	"github.com/hashicorp/go-version"
 	"github.com/ktr0731/dept/cmd"
 	"github.com/mitchellh/cli"
 )
 
 func main() {
+	ver := version.Must(version.NewVersion(strings.TrimPrefix(runtime.Version(), "go")))
+	if !isCompatibleGoVersion(ver) {
+		fmt.Fprintf(os.Stderr, "dept needs Go 1.11 or later")
+		os.Exit(1)
+	}
+
+	if isLimitedGoModSupport(ver) {
+		const key = "GO111MODULE"
+		oldVal := os.Getenv(key)
+		os.Setenv(key, "on")
+		defer func() {
+			os.Setenv(key, oldVal)
+		}()
+	}
+
 	app := cli.NewCLI("dept", "0.1.0")
 	app.Commands = map[string]cli.CommandFactory{
-		"add": cmd.Add,
+		"get": cmd.Get,
 	}
 	app.Args = os.Args[1:]
 	code, err := app.Run()
@@ -20,4 +39,22 @@ func main() {
 	}
 
 	os.Exit(code)
+}
+
+var minVersion = version.Must(version.NewVersion("1.11.0"))
+
+// isCompatibleGoVersion checks whether the Go version has Go modules support.
+// version must be formatted as semver form.
+func isCompatibleGoVersion(v *version.Version) bool {
+	return v.Compare(minVersion) >= 0
+}
+
+// isLimitedGoModSupport checks whether the Go version is limited support for Go modules.
+// If Go modules incompatible version passed, isLimitedGoModSupport will panic.
+func isLimitedGoModSupport(v *version.Version) bool {
+	if !isCompatibleGoVersion(v) {
+		panic("Go modules incompatible version passed")
+	}
+	minor := v.Segments()[1]
+	return minor == 11
 }
---
+++ main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"testing"
+
+	version "github.com/hashicorp/go-version"
+)
+
+func Test_isCompatibleGoVersion(t *testing.T) {
+	cases := []struct {
+		version    string
+		compatible bool
+	}{
+		{version: "1.10.0", compatible: false},
+		{version: "1.11.0", compatible: true},
+		{version: "1.20.0", compatible: true},
+	}
+
+	for _, c := range cases {
+		ver := version.Must(version.NewVersion(c.version))
+		if c.compatible != isCompatibleGoVersion(ver) {
+			t.Error("isCompatibleGoVersion must be return same boolean value as c.compatible")
+		}
+	}
+}
+
+func Test_isLimitedGoModSupport(t *testing.T) {
+	cases := []struct {
+		version   string
+		limited   bool
+		willPanic bool
+	}{
+		{version: "1.10.0", willPanic: true},
+
+		{version: "1.11.0", limited: true},
+		{version: "1.11.1", limited: true},
+		{version: "1.12.0", limited: false},
+	}
+
+	for _, c := range cases {
+		t.Run(c.version, func(t *testing.T) {
+			ver := version.Must(version.NewVersion(c.version))
+			if c.willPanic {
+				defer func() {
+					err := recover()
+					if err == nil {
+						t.Error("must panic because incompatible version passed")
+					}
+				}()
+			}
+
+			if c.limited != isLimitedGoModSupport(ver) {
+				t.Error("isLimitedGoModSupport must be return same boolean value as c.limited")
+			}
+		})
+	}
+}
